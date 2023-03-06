dae1263bdcacd7303817d9fd6a547efd2782e263
Antony Alen <antonyalen1960@gmail.com>
2023-03-06 13:16:15 +0300

    update

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+import uvicorn
+import requests
+import json
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Status": "Working"}
+
+
+@app.post("/api/{token}/{status}")
+def create_item(token: str, status: str):
+    
+    url = "https://aws-demo.razorinformatics.co.ke/api/v1/sensors"
+    payload = json.dumps({"status": status})
+    headers = {
+    'Accept': 'application/json',
+    'Authorization': f'Bearer {token}',
+    'Content-Type': 'application/json'
+    }
+    
+    response = requests.request("POST", url, headers=headers, data=payload)
+    
+    return response.json()
+
+
+if __name__ == "__main__":
+    config = uvicorn.Config("main:app", port=5000, log_level="info", reload=True)
+    server = uvicorn.Server(config)
+    server.run()
+
