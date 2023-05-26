264b82db8a7fc4fd87ac052b3e9b2ede210f20dd
Antony Alen <antonyalen1960@gmail.com>
2023-05-26 17:39:12 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,39 +1,94 @@
 import socket
+from threading import Thread
+from time import strftime, gmtime
 import struct
 import binascii
 
-PORT = 6543
+# Global Variables
+host = '0.0.0.0'
+port = 6543
 
 
-# Set up a TCP/IP server
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# Bind the socket to server address and port 81
-server_address = ('0.0.0.0', PORT)
-tcp_socket.bind(server_address)
+class ClientThread(Thread):
+    def __init__(self, _socket) -> None:
+        Thread.__init__(self)
+        self.conn = _socket[0]
+        self.addr = _socket[1]
+        self.identifier = "None"
+        self.logTime = strftime('%d %b %H:%M:%S', gmtime())
+        self.step = 1
+        self.imei = "unknown"
 
-# Listen on port 81
-tcp_socket.listen(1)
+    def log(self, msg):
+        print(f"{self.logTime}\t{self.identifier}\t{msg}")
+        pass
 
-while True:
-    print("Waiting for connection")
-    connection, client = tcp_socket.accept()
+    def run(self):
+        client = self.conn
+        if client:
+            self.identifier = self.addr[0]
+            for _ in range(2):
+                try:
+                    buff = self.conn.recv(8192)
+                    received = binascii.hexlify(buff)
+                    received_sepd = binascii.hexlify(buff, " ")
+                    
+                    print(f"[DEBUG]: DATA RECIEVED: {received_sepd}")
+                    
+                    if len(received) > 2:
+                        if self.step == 1:
+                            self.step = 2
+                            self.imei = received
+                            self.log("Device Authenticated | IMEI: {}".format(self.imei))
+                            self.conn.send('\x01'.encode('utf-8'))
+                        elif self.step == 2:
+                            decoder = Decoder(payload=received, imei=self.imei)
+                            len_records = decoder.decode_data()
+                            if len_records == 0:
+                                self.conn.send('\x00'.encode('utf-8'))
+                                self.conn.close()
+                            else:
+                                self.conn.send(struct.pack("!L", len_records))
+                                self.log("Done! Closing Connection")
+                                self.conn.close()
+                    else:
+                        self.conn.send('\x00'.encode('utf-8'))
+                except socket.error as err:
+                    print(f"[+] Socket Error: {err}")
+        else:
+            self.log('Socket is null')
+
+
+class Decoder:
+    
+    def __init__(self, payload, imei):
+        pass
+    
+    def decode_data(self):
+        return None
+
+
+if __name__ == "__main__":
+    print(f"SERVER. {strftime('%d %b %H:%M:%S', gmtime())}")
+    print(f"Server Started at port: {port}")
+    server = None
     try:
-        print('Connection from', client)
-        while True:
-            data = connection.recv(1280)
-            data = binascii.hexlify(data)
-            # data = data.decode("utf-8")
-            print(f'Received : ', data)
-            if data:
-                # if data.strip().isdigit():
-                #     device_id = int(data.strip())
-                #     print("Device ID: ", device_id)
-                connection.sendall("\01".encode('utf-8'))
-            else:
-                break
-    finally:
-        # Clean up the connection
-        connection.close()
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    except socket.error as e:
+        print("[X] Socket Creation Error: {}".format(e))
+        exit(0)
 
+    try:
+        server.bind((host, port))
+    except socket.error as e:
+        print("[X] Socket Binding Error: {}".format(e))
+        exit(0)
 
+    try:
+        server.listen(5)
+    except socket.error as e:
+        print("[X] Socket Listening Error: {}".format(e))
+        exit(0)
+    while True:
+        ClientThread(server.accept()).start()
