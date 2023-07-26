6411a619a9bcd3c8af954fe3d4f52df37a68eade
Antony Alen <antonyalen1960@gmail.com>
2023-07-26 13:42:15 +0300

    New decoder

--- server_general.py
+++ server_general.py
@@ -7,7 +7,7 @@ import binascii
 from pprint import pprint
 import json
 from datetime import datetime
-
+from decoder import CodecKT1Decoder
 
 # Global Variables
 host = '0.0.0.0'
@@ -26,23 +26,68 @@ class ClientThread(Thread):
         self.step = 1
         self.imei = "unknown"
         self.start_time = time.time()
+        self.server_id = "3xVGBzjEJIR9xAj7"
 
     def run(self):
         print(f"[SERVER] NEW CONNECTION from: {self.addr[0]}")
         client = self.conn
         if client:
             self.identifier = self.addr
-            for _ in range(5):
+            for _ in range(1):
                 try:
-                    
+                    decoder = CodecKT1Decoder()
                     buff = self.conn.recv(8192)
-                    received = binascii.hexlify(buff)
-                    received_sepd = binascii.hexlify(buff, " ")
+                    received = buff.decode('utf-8').strip()
+                    received_sepd = ' '.join(received[i:i+2] for i in range(0, len(received), 2))
                     
                     print(f"\n[DEBUG] [{self.addr[0]}] [{datetime.now()}]: DATA RECIEVED RAW: {buff}")
                     # print(f"\n[DEBUG] [{self.addr[0]}] [{datetime.now()}]: DATA RECIEVED: {received_sepd}")
+                    
+                    ## Split the data packet according to the packet format.
+                    packet_structure = decoder.decode_packet_structure(received)
+                    print(json.dumps(packet_structure, indent=2))
+                    
+                    ## Verify the structure of the packet
+                    if packet_structure.get('start_bit') == 'EEEE' and packet_structure.get('stop_bit') == 'AAAA':
+                        ## Found a login packet, create acknowledgement packet and send to device.
+                        print(f"[DEBUG] [{self.addr[0]}] [{datetime.now()}]: Found a valid packet, running conditional protocol according to protocol number...")
+                        
+                        ## Login Message
+                        if packet_structure.get('protocol_number') == '01':
+                            
+                            imei, device_id = decoder.login_packet_decoder(received)
+                            imei = imei.decode('utf-8')
+                            device_id = device_id.decode('utf-8')
+                            
+                            print(f"[DEBUG] [{self.addr[0]}] [{datetime.now()}] [PROTOCOL NUMBER={packet_structure.get('protocol_number')}]: Is a login handshake. from IMEI: {imei}")
+                            print(f"[SERVER] [{self.addr[0]}] [{datetime.now()}] Valid connection from: {device_id}")
+                            
+                            # Achknowledge login and send responce to device
+                            response_packet = decoder.acknowledgement_packet_encoder(self.server_id, device_id)
+                            response_packet += b"\n\r"
+                            # self.conn.send(response_packet)
+                            self.conn.send(f"FROM SERVER!!\n\r".encode('utf-8'))
+                            print(f'[DEBUG] [{self.addr[0]}] [{datetime.now()}]: Sent response: {response_packet}')
+                            
+                            # Save the imei number
+                            self.imei = imei
+                        
+                        elif packet_structure.get('protocol_number') == '02':
+                            
+                            device_data = decoder.device_data_decoder(received)
+                            
+                            print(f"[DEBUG] [{self.addr[0]}] [{datetime.now()}] [PROTOCOL NUMBER = {packet_structure.get('protocol_number')}]: Is a login handshake. from IMEI: {self.imei}")
+                            print(f"[SERVER] [{self.addr[0]}] [{datetime.now()}] Device data: \n{json.dumps(device_data, indent=2)}")
+                            
+                            # Achknowledge login and send responce to device
+                            response_packet = f"{packet_structure.get('error_ckeck')}\n\r".encode('utf-8')
+                            self.conn.send(response_packet)
+                            print(f'[DEBUG] [{self.addr[0]}] [{datetime.now()}]: Sent response: {response_packet}')
+                        
+                    else:
+                        print(f"[DEBUG] [{self.addr[0]}] [{datetime.now()}]: Found a INVALID packet, Exiting.")
+                        self.conn.close()
 
-                    self.conn.send(f"FROM SERVER: I recieved :: {buff}\n\r".encode("utf-8"))
         
                 except socket.error as err:
                     print(f"[SERVER] [ERROR] [{self.addr[0]}] [{datetime.now()}] Socket Error: {err}")
