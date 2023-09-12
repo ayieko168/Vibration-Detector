c66ff4d069f66148ac53ab67ea8dce1bdf5c1aa9
Antony Alen <antonyalen1960@gmail.com>
2023-09-12 19:01:36 +0300

    cleanup

--- GSMTester/GSMTester.ino
+++
@@ -1,9 +0,0 @@
-#include <Wire.h>
-#include <SoftwareSerial.h>
-
-char* imei = "NULL0";
-String deviceLoginPacketString = "NULL";
-const int BOARD_RESET_PIN = 2;
-String serverResponse;
-int connectedToInterent;
-
